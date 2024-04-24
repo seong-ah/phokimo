f65eeb31695f9da8a6cf0d211a443d178d8acce2
Gereon Feldmann <feldmann.gereon@gmail.com>
2024-04-24 13:47:43 +0200

    Add read the docs style.

--- docs/conf.py
+++ docs/conf.py
@@ -38,6 +38,7 @@ extensions = ["sphinx.ext.todo",
             "sphinx.ext.autosummary",
             "sphinx.ext.napoleon",
             "sphinx.ext.autodoc",
+            'sphinx_rtd_theme',
             ]
 
 templates_path = ['_templates']
@@ -47,5 +48,5 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
